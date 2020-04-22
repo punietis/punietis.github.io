3c8c8862965aa3f938ce8b8b52c53e102ad19efb
punietis <pauline.unietis@gmail.com>
2020-04-21 21:15:57 -0700

    Updates

---
+++ precache-manifest.2ff33711f0623fc9f8c3bf0f7db73482.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2304a8f7bfece389c48f68c7f47bb1",
+    "url": "/bowdoin-facility-hours/index.html"
+  },
+  {
+    "revision": "9c697d3e8c599aeae867",
+    "url": "/bowdoin-facility-hours/static/css/2.bd4f2bea.chunk.css"
+  },
+  {
+    "revision": "cadb10c020db9328c541",
+    "url": "/bowdoin-facility-hours/static/css/main.870a0065.chunk.css"
+  },
+  {
+    "revision": "9c697d3e8c599aeae867",
+    "url": "/bowdoin-facility-hours/static/js/2.bed3ecd0.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/bowdoin-facility-hours/static/js/2.bed3ecd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cadb10c020db9328c541",
+    "url": "/bowdoin-facility-hours/static/js/main.321815a6.chunk.js"
+  },
+  {
+    "revision": "77f26e2ff5025231a812",
+    "url": "/bowdoin-facility-hours/static/js/runtime-main.81350abd.js"
+  }
+]);
